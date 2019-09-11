5c543eaa72672ac69610ff64211ba7e108d32fa2
allison <allison.dixon@ssfs.org>
2019-09-11 08:54:14 -0400

    Made some shapes

---
+++ shapes.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.penup()
+turtle.forward(200)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+turtle.left(120)
+turtle.forward(200)
+
+turtle.exitonclick()
