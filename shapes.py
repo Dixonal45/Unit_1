052ea43b1eeb32ab534a9fbe9812c6971d855a83
allison <allison.dixon@ssfs.org>
2019-09-12 10:12:36 -0400

    Made some shapes

--- shapes.py
+++ shapes.py
@@ -1,22 +1,165 @@
 import turtle
+turtle.speed(0)
+turtle.pencolor("green")
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+turtle.penup()
+turtle.forward(200)
+turtle.pencolor("purple")
+turtle.pendown()
 
 turtle.forward(100)
-turtle.left(120)
+turtle.left(90)
 turtle.forward(100)
-turtle.left(120)
+turtle.left(90)
 turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
 turtle.penup()
 turtle.forward(200)
+turtle.pencolor("red")
 turtle.pendown()
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
+
+for x in range(5):
+    turtle.forward(50)
+    turtle.left(72)
+
 turtle.penup()
-turtle.left(120)
+turtle.forward(140)
+turtle.pencolor("blue")
+turtle.pendown()
+
+turtle.circle(20)
+
+turtle.right(90)
+turtle.penup()
+turtle.forward(100)
+turtle.pencolor("orange")
+turtle.pendown()
+
+for x in range(5):
+    turtle.forward(80)
+    turtle.right(144)
+
+turtle.penup()
 turtle.forward(200)
+turtle.pencolor("pink")
+turtle.pendown()
+
+for x in range (4):
+    turtle.forward(60)
+    turtle.right(90)
+turtle.right(30)
+for x in range (4):
+    turtle.forward(60)
+    turtle.right(90)
+turtle.right(30)
+for x in range (4):
+    turtle.fd(60)
+    turtle.right(90)
+turtle.right(30)
+for x in range (4):
+    turtle.fd(60)
+    turtle.right(90)
+turtle.right(30)
+for x in range(4):
+    turtle.fd(60)
+    turtle.right(90)
+
+turtle.penup()
+turtle.goto(-100,100)
+turtle.rt(60)
+turtle.pencolor("turquoise")
+turtle.pendown()
+
+for x in range(4):
+    turtle.fd(50)
+    turtle.lt(120)
+turtle.rt(90)
+for x in range(3):
+    turtle.fd(50)
+    turtle.lt(90)
+
+turtle.penup()
+turtle.goto(300,300)
+turtle.pencolor("black")
+turtle.rt(300)
+turtle.pendown()
+
+turtle.color("red")
+turtle.begin_fill()
+for x in range(4):
+    turtle.fd(50)
+    turtle.lt(120)
+turtle.end_fill()
+turtle.rt(90)
+turtle.color("purple")
+turtle.begin_fill()
+for x in range(3):
+    turtle.fd(50)
+    turtle.lt(90)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(220,300)
+turtle.seth(240)
+turtle.pendown()
+
+turtle.color("red")
+turtle.begin_fill()
+for x in range(4):
+    turtle.fd(50)
+    turtle.lt(120)
+turtle.end_fill()
+turtle.rt(90)
+turtle.color("purple")
+turtle.begin_fill()
+for x in range(3):
+    turtle.fd(50)
+    turtle.lt(90)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(140,300)
+turtle.seth(240)
+turtle.pendown()
+
+turtle.color("red")
+turtle.begin_fill()
+for x in range(4):
+    turtle.fd(50)
+    turtle.lt(120)
+turtle.end_fill()
+turtle.rt(90)
+turtle.color("purple")
+turtle.begin_fill()
+for x in range(3):
+    turtle.fd(50)
+    turtle.lt(90)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(60,300)
+turtle.seth(240)
+turtle.pendown()
+
+turtle.color("red")
+turtle.begin_fill()
+for x in range(4):
+    turtle.fd(50)
+    turtle.lt(120)
+turtle.end_fill()
+turtle.rt(90)
+turtle.color("purple")
+turtle.begin_fill()
+for x in range(3):
+    turtle.fd(50)
+    turtle.lt(90)
+turtle.end_fill()
 
 turtle.exitonclick()
