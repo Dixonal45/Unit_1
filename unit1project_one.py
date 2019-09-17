df6982ef0ddf5de3b3fd2862cbe7c6004a08cfdc
allison <allison.dixon@ssfs.org>
2019-09-17 13:53:44 -0400

    Finished Unit 1 assignment and fixed errors

--- unit1project_one.py
+++ unit1project_one.py
@@ -1,20 +1,23 @@
 # unit1project_one.py
 # by Allison Dixon
-# Last modified September 16, 2019
+# Last modified September 17, 2019
 #
 # This program makes four different colored octagons
 
 import turtle
 
 
-# Creates an octagon
-def make_an_octagon():
+# This function creates an octagon in color of choice
+def make_an_octagon(color):
+    turtle.color(color)
+    turtle.begin_fill()
     for x in range(8):
         turtle.fd(50)
         turtle.rt(45)
+    turtle.end_fill()
 
 
-# Makes turtle go to set location
+# This function makes turtle go to location of choice
 def go_to(x, y):
     turtle.penup()
     turtle.goto(x, y)
@@ -23,31 +26,19 @@ def go_to(x, y):
 
 # Makes a cyan filled octagon
 go_to(-100, -100)
-turtle.color("cyan")
-turtle.begin_fill()
-make_an_octagon()
-turtle.end_fill()
+make_an_octagon("cyan")
 
 # Makes a medium purple filled octagon in a new location
 go_to(-100, 100)
-turtle.color("medium purple")
-turtle.begin_fill()
-make_an_octagon()
-turtle.end_fill()
+make_an_octagon("medium purple")
 
 # Makes a lavender filled octagon in a new location
 go_to(100, 100)
-turtle.color("lavender")
-turtle.begin_fill()
-make_an_octagon()
-turtle.end_fill()
+make_an_octagon("lavender")
 
 # Makes a dodger blue filled octagon in a new location
 go_to(100, -100)
-turtle.color("dodger blue")
-turtle.begin_fill()
-make_an_octagon()
-turtle.end_fill()
+make_an_octagon("dodger blue")
 
 # Program stays on screen until user clicks on screen
 turtle.exitonclick()
